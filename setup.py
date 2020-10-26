5ce18adcf3e466488aaff5cd6ceac17d9a2029e5
Dyllan McCreary <mccreary@dyllan.ai>
2020-10-26 01:57:13 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="rigl-torch",
     version="0.1",
+    author="Dyllan McCreary",
+    author_email="mccreary@dyllan.ai",
+    description="Implementation of Google Research's \"RigL\" sparse model training method in PyTorch.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/McCrearyD/rigl-torch",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
