709a07e8d73e5613694ee3c8530ee9aa0c0028a6
McCrearyD <mccreary@dyllan.ai>
2020-10-25 19:42:05 -0700

    fixed handling of linear layers & uniform sparsity
    no major reworks, just better for printing stats

--- rigl_torch/util.py
+++ rigl_torch/util.py
@@ -2,21 +2,23 @@ import torch
 import torchvision
 
 
-def get_conv_layers_with_activations(model, i=0, layers=None, identity_indices=None, ignore_linear_layers=False):
+def get_conv_layers_with_activations(model, i=0, layers=None, linear_layers_mask=None):
     if layers is None:
         layers = []
-    if identity_indices is None:
-        identity_indices = []
+    if linear_layers_mask is None:
+        linear_layers_mask = []
 
     for layer_name, p in model._modules.items():
         if isinstance(p, torch.nn.Conv2d):
             layers.append([p])
+            linear_layers_mask.append(0)
             i += 1
         # elif isinstance(p, torch.nn.AdaptiveAvgPool2d):
             # layers.append([p])
             # i += 1
-        elif isinstance(p, torch.nn.Linear) and not ignore_linear_layers:
+        elif isinstance(p, torch.nn.Linear):
             layers.append([p])
+            linear_layers_mask.append(1)
         elif isinstance(p, torch.nn.BatchNorm2d):
             layers[-1].append(p)
         elif isinstance(p, torch.nn.ReLU):
@@ -25,21 +27,27 @@ def get_conv_layers_with_activations(model, i=0, layers=None, identity_indices=N
             layers[-1].append(p)
         elif layer_name == 'downsample':
             layers.append(p)
+            linear_layers_mask.append(0)
         elif isinstance(p, torchvision.models.resnet.Bottleneck) or isinstance(p, torchvision.models.resnet.BasicBlock):
-            if hasattr(p, 'downsample') and p.downsample is not None:
-                identity_indices.append(i)
-            _, identity_indices, i = get_conv_layers_with_activations(p, i=i, layers=layers, identity_indices=identity_indices)
+            # if hasattr(p, 'downsample') and p.downsample is not None:
+                # identity_indices.append(i)
+            _, linear_layers_mask, i = get_conv_layers_with_activations(p, i=i, layers=layers, linear_layers_mask=linear_layers_mask)
         else:
-            _, identity_indices, i = get_conv_layers_with_activations(p, i=i, layers=layers, identity_indices=identity_indices)
+            _, linear_layers_mask, i = get_conv_layers_with_activations(p, i=i, layers=layers, linear_layers_mask=linear_layers_mask)
 
-    return layers, identity_indices, i 
+    return layers, linear_layers_mask, i 
 
 
-def get_W(model, ignore_linear_layers=False):
-    layers, _, _ = get_conv_layers_with_activations(model, ignore_linear_layers=ignore_linear_layers)
+def get_W(model, return_linear_layers_mask=False):
+    layers, linear_layers_mask, _ = get_conv_layers_with_activations(model)
 
     W = []
     for layer in layers:
         idx = 0 if hasattr(layer[0], 'weight') else 1
         W.append(layer[idx].weight)
+
+    assert len(W) == len(linear_layers_mask)
+
+    if return_linear_layers_mask:
+        return W, linear_layers_mask
     return W
