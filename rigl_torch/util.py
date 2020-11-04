f2ed1e589e116918cde16d39d4e77cd12f3e2318
McCrearyD <mccreary@dyllan.ai>
2020-11-03 19:24:14 -0800

    changed the way get_W is done, allows more general usage

--- rigl_torch/util.py
+++ rigl_torch/util.py
@@ -2,23 +2,24 @@ import torch
 import torchvision
 
 
-def get_conv_layers_with_activations(model, i=0, layers=None, linear_layers_mask=None):
+def get_weighted_layers(model, i=0, layers=None, linear_layers_mask=None):
+    print(i)
     if layers is None:
         layers = []
     if linear_layers_mask is None:
         linear_layers_mask = []
 
-    for layer_name, p in model._modules.items():
-        if isinstance(p, torch.nn.Conv2d):
-            layers.append([p])
-            linear_layers_mask.append(0)
-            i += 1
-        # elif isinstance(p, torch.nn.AdaptiveAvgPool2d):
-            # layers.append([p])
-            # i += 1
-        elif isinstance(p, torch.nn.Linear):
+    items = model._modules.items()
+    if i == 0:
+        items = [(None, model)]
+
+    for layer_name, p in items:
+        if isinstance(p, torch.nn.Linear):
             layers.append([p])
             linear_layers_mask.append(1)
+        elif hasattr(p, 'weight'):
+            layers.append([p])
+            linear_layers_mask.append(0)
         elif isinstance(p, torch.nn.BatchNorm2d):
             layers[-1].append(p)
         elif isinstance(p, torch.nn.ReLU):
@@ -29,17 +30,16 @@ def get_conv_layers_with_activations(model, i=0, layers=None, linear_layers_mask
             layers.append(p)
             linear_layers_mask.append(0)
         elif isinstance(p, torchvision.models.resnet.Bottleneck) or isinstance(p, torchvision.models.resnet.BasicBlock):
-            # if hasattr(p, 'downsample') and p.downsample is not None:
-                # identity_indices.append(i)
-            _, linear_layers_mask, i = get_conv_layers_with_activations(p, i=i, layers=layers, linear_layers_mask=linear_layers_mask)
+            _, linear_layers_mask, i = get_weighted_layers(p, i=i + 1, layers=layers, linear_layers_mask=linear_layers_mask)
         else:
-            _, linear_layers_mask, i = get_conv_layers_with_activations(p, i=i, layers=layers, linear_layers_mask=linear_layers_mask)
+            _, linear_layers_mask, i = get_weighted_layers(p, i=i + 1, layers=layers, linear_layers_mask=linear_layers_mask)
 
     return layers, linear_layers_mask, i 
 
 
+
 def get_W(model, return_linear_layers_mask=False):
-    layers, linear_layers_mask, _ = get_conv_layers_with_activations(model)
+    layers, linear_layers_mask, _ = get_weighted_layers(model)
 
     W = []
     for layer in layers:
